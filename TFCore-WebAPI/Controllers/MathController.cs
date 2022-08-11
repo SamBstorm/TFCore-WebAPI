c03fa9192b9c5993430567286210427882d227ac
Legrain Samuel <samuel.legrain@bstorm.be>
2022-08-11 16:55:28 +0200

    Ajoutez des fichiers projet.

---
+++ TFCore-WebAPI/Controllers/MathController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TFCore_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    //[Route("mathematique")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public string Welcome()
+        {
+            return "Bienvenu sur mon API de mathématique!";
+        }
+
+        [HttpGet("info")]
+        public string Info()
+        {
+            string message = "Cette application à pour but de vous apprendre les mathématiques à l'aide d'une API codé en .net 6 .";
+            return message;
+        }
+
+        [HttpGet("Addition")]
+        public IActionResult AdditionWithParam(double a = 5, double b = 5) 
+        {
+            return Addition(a, b);
+        }
+
+        [HttpGet("{a}/plus/{b}")]
+        public IActionResult Addition(double a = 5, double b = 5)
+        {
+            return Ok(a + b);
+        }
+        [HttpGet("Soustraction")]
+        public IActionResult Soustraction(double a = 5, double b = 5)
+        {
+            return Ok(a - b);
+        }
+        [HttpGet("Multiplication")]
+        public IActionResult Multiplication(double a = 5, double b = 5)
+        {
+            return Ok(a * b);
+        }
+        [HttpGet("Division")]
+        public IActionResult Division(double a = 5, double b = 5)
+        {
+            try
+            {
+                if (b == 0) throw new ArgumentException($"Le diviseur ne peut pas être égale à 0, cela tend vers l'infini");
+                return Ok(a / b);
+            }
+            catch (ArgumentException e)
+            {
+                return BadRequest(new { message = e.Message, a = a, b = b });
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
